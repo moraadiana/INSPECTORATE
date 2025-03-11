6a18545ebaf543a33b75c611f6783da2f4cd6e39
moraadiana <moraadiana13@gmail.com>
2025-03-11 12:47:32 +0300

    update

---
+++ OperationsPortal/OperationsPortal/Models/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OperationsPortal.Models
+{
+    public class Account
+    {
+        public string custNo { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+
+    }
+}
---
+++ OperationsPortal/OperationsPortal/Models/OTP.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OperationsPortal.Models
+{
+    public class OTP
+    {
+        public string OTPCode { get; set; }
+
+    }
+}
